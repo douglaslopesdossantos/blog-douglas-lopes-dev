e1a0a3bb35793e417ca1a328cc8a75e3ca592ced
douglaslopesdossantos <contato.geardev@gmail.com>
2025-10-02 19:43:41 -0300

    chore: alter package

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,8 +3,8 @@ import ExtLink from './ext-link'
 export default function Footer() {
   return (
     <>
-      <footer>
-        {/* <span>Deploy your own!</span>
+      {/* <footer>
+        <span>Deploy your own!</span>
         <ExtLink href="https://vercel.com/new/git/external?repository-url=https://github.com/ijjk/notion-blog/tree/main&project-name=notion-blog&repository-name=notion-blog">
           <img
             src="https://vercel.com/button"
@@ -18,8 +18,8 @@ export default function Footer() {
           <ExtLink href="https://github.com/ijjk/notion-blog">
             view source
           </ExtLink>
-        </span> */}
-      </footer>
+        </span>
+      </footer> */}
     </>
   )
 }
